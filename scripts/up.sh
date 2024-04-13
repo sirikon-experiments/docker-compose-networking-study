19b30ca3459d7c8f7bca7a95c92699f571385318
Carlos Fdez. Llamas <hello@sirikon.me>
2024-04-13 17:09:49 +0200

    wip

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 (cd "image" && docker build -t study .)
 (cd "projects/alpha" && docker compose up -d)
 (cd "projects/beta" && docker compose up -d)
-(cd "projects/omega" && docker compose up -d)
+(cd "projects/gamma" && docker compose up -d)
